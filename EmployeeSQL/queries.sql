82e610d7582a5b82449261627223a47e55714414
Rob397 <105466612+Rob397@users.noreply.github.com>
2022-07-09 15:48:46 +1000

    Update queries.sql

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -74,5 +74,9 @@ WHERE (dept_name = 'Sales' OR dept_name ='Development');
 -- 8. In descending order, list the frequency count of employee last names,
 --  i.e., how many employees share each last name.
 
-SELECT COUNT(DISTINCT(Employees.last_name))
+SELECT DISTINCT(Employees.last_name)
+FROM Employees
+ORDER BY last_name DESC;
+
+SELECT COUNT(DISTINCT Employees.last_name)
 FROM Employees
